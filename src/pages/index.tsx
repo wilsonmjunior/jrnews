7c863979e8e4d8554836bd657b2c046ecc92b5e5
Wilson M Jr <wilsonmjuniorx@gmail.com>
2022-01-18 14:26:31 -0300

    feat: add products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const product = {
     priceId: price.id,
-    amount: Intl.NumberFormat('en-US', {
+    amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
     }).format(price.unit_amount / 100),
