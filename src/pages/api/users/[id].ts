96875d31aab303df1e093c65ab95fa45edda7d4e
Wilson M Jr <wilsonmjuniorx@gmail.com>
2022-01-18 14:26:50 -0300

    feat: add users

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
+  const users = [
+    { id:1, name: 'John Doe 1' },
+    { id:2, name: 'John Doe 2' },
+    { id:3, name: 'John Doe 3' },
+  ]
+
+  const userFounded = users.find(user => user.id === Number(request.query.id));
+
+  return response.json(userFounded)
+}
---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id:1, name: 'John Doe 1' },
+    { id:2, name: 'John Doe 2' },
+    { id:3, name: 'John Doe 3' },
+  ]
+
+  return response.json(users)
+}
