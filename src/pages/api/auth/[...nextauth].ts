7149502a6a8581a7ef19d0322e291cc14b6b98ed
Wilson M Jr <wilsonmjuniorx@gmail.com>
2022-01-19 09:00:52 -0300

    feat: save user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,41 @@ export default NextAuth({
       },
     }),
   ],
+  // jwt: {
+  //   secret: process.env.JWT_SECRET,
+  // },
+  callbacks: {
+    async signIn({ user }) {
+      // { user, account, profile, credentials }
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(email)
+              )
+            )
+          ),
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+  }
 })
