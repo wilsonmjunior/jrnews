6601e8c2b59bab7db535f633651246a9d61c1a1f
Wilson M Jr <wilsonmjuniorx@gmail.com>
2022-01-20 10:59:58 -0300

    feat: create subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/react'
+
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +10,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      // create checkout session
+      const response = await api.post('/checkout/subscribe')
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+      stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
