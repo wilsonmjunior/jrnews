8fc203a7686480cd0de16b96045c5f81e6e4c1f6
Wilson Junior <wilson.junior@merx.tech>
2023-02-20 21:36:10 -0300

    fat: create active link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink';
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
 
 export function Header() {
-  return (
+  return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="" />
+        <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a href="#" className={styles.active}>Home</a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <p>Home</p>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <p>Posts</p>
+          </ActiveLink>
         </nav>
-
         <SignInButton />
       </div>
     </header>
-  )
+  );
 }
